ea8b4219bc6afcbdfc0ac22b9f010bd02d526db3
IFMobile <ifmobilefoz@gmail.com>
2018-09-25 16:40:43 -0300

    Modificações para versão 2

--- app/src/main/java/com/example/professor/threads/MainActivity.java
+++ app/src/main/java/com/example/professor/threads/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtStatus;
     private Button btnProcessar;
+    private ProgressBar pgbProgresso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +22,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         txtStatus = findViewById(R.id.txtStatus);
         btnProcessar = findViewById(R.id.btnProcessar);
+        pgbProgresso = findViewById(R.id.pgbProgresso);
 
         btnProcessar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        txtStatus.setText(R.string.processando);
+        //txtStatus.setText(R.string.processando);
+        btnProcessar.setEnabled(false);
+        pgbProgresso.setVisibility(View.VISIBLE);
+        txtStatus.setVisibility(View.INVISIBLE);
         executarAlgoDemorado();
-        txtStatus.setText(R.string.finalizado);
+        //txtStatus.setText(R.string.finalizado);
     }
 
 
     private void executarAlgoDemorado() {
-        SystemClock.sleep(15000);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(15000);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        pgbProgresso.setVisibility(View.INVISIBLE);
+                        btnProcessar.setEnabled(true);
+                        txtStatus.setText(R.string.finalizado);
+                        txtStatus.setVisibility(View.VISIBLE);
+                    }
+                });
+
+            }
+        }).start();
     }
 }
